8c526b8e0033bb567a8639bc12c6191209f2add1
Luis Herrera <luis1818.lh@gmail.com>
2023-08-29 16:10:15 -0500

    En la class Courses y Student, se incluye el metodo get an set

--- vPoo/main.js
+++ vPoo/main.js
@@ -78,6 +78,67 @@ const miguelit01 = {
 
 // objetos usando Prototipos con la sintaxis de Class
 
+class Course {
+    constructor({
+        name,
+        classes = [],
+    }) {
+        this._name = name;
+        this.classes = classes;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(nuevoNombresito) {
+        if (nuevoNombresito === "Curso Malito de programación básica") {
+            console.error('Web... no');
+        } else {
+            this._name =nuevoNombresito;      
+        }
+        
+    }
+}
+
+const cursoProgBasica = new Course ({
+    name: "Curso gratis de programación básica",
+
+})
+const cursoDefHtml = new Course ({
+    name: "Curso definitivo de HTML y CSS",
+
+})
+
+const cursoPracHtml = new Course ({
+    name: "Curso practico de HTML y CSS",
+
+})
+const cursoDtBusiness = new Course ({
+    name: "Curso DataBusiness",
+
+})
+const cursoDtViz = new Course ({
+    name: "Curso DataViz",
+
+})
+const cursoTableau = new Course ({
+    name: "Curso de Tableau",
+
+})
+
+const cursoIntroVg = new Course ({
+    name: "Curso de introducción a la Produccion de videojuegos",
+
+})
+
+const cursoUnreal = new Course ({
+    name: "Curso de Unreal Engine",
+
+})
+const cursoUnity = new Course ({
+    name: "Curso de Unity 3D",
+
+})
+
 class LearningPaths {
     constructor({
         name,
@@ -91,31 +152,29 @@ class LearningPaths {
 const dWeb = new LearningPaths( {
     name: 'Escuela de Desarrollo Web',
     courses: [
-        'Curso Definitivo de HTML y CSS',
-        'Curso Práctico de HTML y CSS',
-        'Curso de Responsive Design'
+        cursoProgBasica,
+        cursoDefHtml,
+        cursoPracHtml,
     ],
 })
 
 const dSciences = new LearningPaths( {
     name: 'Escuela de Data Science',
     courses: [
-        'Curso DataBusiness',
-        'Curso DataViz',
-        'Curso de Tableau',
+        cursoDtBusiness,
+        cursoDtViz,
+        cursoTableau,
     ],
 })
 
 const vGames = new LearningPaths( {
     name: 'Escuela de Video Juegos',
     courses:[
-        'Curso de introducción a la Produccion de videojuegos',
-        'Curso de Unreal Engine',
-        'Curso de Unity 3D',
+        cursoIntroVg,
+        cursoUnreal,
+        cursoUnity,
     ],
 })
-
-
 
 class Student {
     constructor({
@@ -129,9 +188,9 @@ class Student {
         learningPaths = [],
 
     }) {
-        this.name = name;
-        this.email = email;
-        this.userName = userName;
+        this._name = name;
+        this._email = email;
+        this._userName = userName;
         this.socialMedia = {
             twitter,
             instagram,
@@ -140,6 +199,26 @@ class Student {
         this.aprovedCourses = aprovedCourses;
         this.learningPaths = learningPaths;
     }
+
+    //metodo getter and setter, el gion bajo en el atribujo es para diferenciarlo del método. este ultimo devuelve el valor de los atributos
+    get name(){
+        return this._name;
+    }
+    set name (nuevoNombre){
+        this._name = nuevoNombre;
+    }
+    get email(){
+        return this._email;
+    }
+    set email (nuevoEmail){
+        this._email = nuevoEmail;
+    }
+    get userName(){
+        return this._userName;
+    }
+    set userName (nuevoUserName){
+        this._userName = nuevoUserName;
+    }
 }
 
 const luis2 = new Student( {
@@ -147,7 +226,10 @@ const luis2 = new Student( {
     userName: 'luher',
     email: 'luis1818.lh@gmail.com',
     twitter: 'luisherrerac',
-    learningPaths: [vGames, dSciences],
+    learningPaths: [
+        dWeb, 
+        dSciences
+    ],
 
 })
 
